d2d0e94d7bc6f288e26153573b364a9a43303ae3
Nadein Michael <nadein.michael@gmail.com>
2023-10-06 14:34:24 +0700

    feat: add arrow game

--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -10,27 +10,21 @@ import movieBig from '../img/projects/movie-big.jpg';
 import employees from '../img/projects/employees.jpg';
 import employeesBig from '../img/projects/employees-big.jpg';
 
+import bike from '../img/projects/bike.jpg';
+import bikeBig from '../img/projects/bike-big.jpg';
+
+import carshering from '../img/projects/carshering.jpg';
+import carsheringBig from '../img/projects/carshering-big.jpg';
+
+import iphone from '../img/projects/iphone.jpg';
+import iphoneBig from '../img/projects/iphone-big.jpg';
+
+import arrowGame from '../img/projects/arrow-game.jpg';
+import arrowGameBig from '../img/projects/arrow-game-big.jpg';
+
 const projects = [
   {
     id: 0,
-    title: 'Airpods lending',
-    skills: 'HTML, CSS, @media, JS',
-    img: airpodsLending,
-    imgBig: airpodsLendingBig,
-    gitHubLink: 'https://github.com/NadeinMichael/airpods-lending',
-    deploy: 'https://nadeinmichael.github.io/airpods-lending/',
-  },
-  {
-    id: 1,
-    title: 'Travel lending',
-    skills: 'HTML, CSS, @media',
-    img: travel,
-    imgBig: travelBig,
-    gitHubLink: 'https://github.com/NadeinMichael/responsive-website-Travel-',
-    deploy: 'https://nadeinmichael.github.io/responsive-website-Travel-/',
-  },
-  {
-    id: 2,
     title: 'Movies explorer App',
     skills: 'HTML, CSS, @media, React, Node js, Express',
     img: movie,
@@ -39,13 +33,67 @@ const projects = [
     deploy: 'https://nadeinmichael.github.io/movies-explorer/',
   },
   {
-    id: 3,
+    id: 1,
     title: 'Employees App',
-    skills: 'React, Node js, Express, Ant design, Redux RTK, TypeScript',
+    skills: 'React, TypeScript, Redux RTK, Node js, Express, Ant design, ',
     img: employees,
     imgBig: employeesBig,
     gitHubLink: 'https://github.com/NadeinMichael/Employees',
     // deploy: 'https://nadeinmichael.github.io/movies-explorer/',
+  },
+  {
+    id: 2,
+    title: 'Arrow Game',
+    skills: 'React, Redux RTK, TypeScript, Material UI, Vitest',
+    img: arrowGame,
+    imgBig: arrowGameBig,
+    gitHubLink: 'https://github.com/NadeinMichael/react-arrow-game',
+    deploy: 'https://nadeinmichael.github.io/react-arrow-game/',
+  },
+  {
+    id: 3,
+    title: 'Airpods lending',
+    skills: 'HTML, CSS, @media, JS',
+    img: airpodsLending,
+    imgBig: airpodsLendingBig,
+    gitHubLink: 'https://github.com/NadeinMichael/airpods-lending',
+    deploy: 'https://nadeinmichael.github.io/airpods-lending/',
+  },
+  {
+    id: 4,
+    title: 'Bike Landing',
+    skills: 'HTML, CSS, @media, JS',
+    img: bike,
+    imgBig: bikeBig,
+    gitHubLink: 'https://github.com/NadeinMichael/bike-landing',
+    deploy: 'https://nadeinmichael.github.io/bike-landing/',
+  },
+  {
+    id: 5,
+    title: 'Carshering Landing',
+    skills: 'HTML, CSS, @media, JS',
+    img: carshering,
+    imgBig: carsheringBig,
+    gitHubLink: 'https://github.com/NadeinMichael/carshering-lending',
+    deploy: 'https://nadeinmichael.github.io/carshering-lending/',
+  },
+  {
+    id: 6,
+    title: 'Iphone Landing',
+    skills: 'HTML, CSS, @media',
+    img: iphone,
+    imgBig: iphoneBig,
+    gitHubLink: 'https://github.com/NadeinMichael/iphone-landing',
+    deploy: 'https://nadeinmichael.github.io/iphone-landing/',
+  },
+  {
+    id: 7,
+    title: 'Travel lending',
+    skills: 'HTML, CSS, @media',
+    img: travel,
+    imgBig: travelBig,
+    gitHubLink: 'https://github.com/NadeinMichael/responsive-website-Travel-',
+    deploy: 'https://nadeinmichael.github.io/responsive-website-Travel-/',
   },
 ];
 
